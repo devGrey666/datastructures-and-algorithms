415988da83ff65c4ec34b5d58cc05f80712bbd0e
devGrey66 <devgrey66@gmail.com>
2023-09-18 01:54:22 +0500

    implements interface

---
+++ src/main/java/org/example/datastructures/linkedLists/LinkedList.java
@@ -0,0 +1,28 @@
+package org.example.datastructures.linkedLists;
+
+public class LinkedList<T> implements ILinkedList<T>{
+    @Override
+    public void add(Object data) {
+
+    }
+
+    @Override
+    public boolean remove(Object data) {
+        return false;
+    }
+
+    @Override
+    public boolean search(Object data) {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
